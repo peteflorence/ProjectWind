fbdcdb82a68150ca6fda37f53960538292d22555
peteflorence <pete.florence@gmail.com>
2015-03-04 10:24:49 -0500

    Separating PlanarQuadPlant (SecondOrderSystem) and DrakeSystem version

--- Quadrotor2D/PlanarQuadPlant_DrakeSystem.m
+++ Quadrotor2D/PlanarQuadPlant_DrakeSystem.m
@@ -1,13 +1,13 @@
-classdef PlanarQuadPlant < SecondOrderSystem
-
-  % state:  
+classdef PlanarQuadPlant_DrakeSystem < DrakeSystem
+  
+  % state:
   %  q(1) - x position
   %  q(2) - z position
   %  q(3) - pitch (theta)
   % input:
   %  u(1) - prop 1 thrust
   %  u(2) - prop 2 thrust
-
+  
   properties  %  based on (Bouadi, Bouchoucha, Tadjine 2007)
     L = 0.25; % length of rotor arm
     m = 0.486; % mass of quadrotor
@@ -22,12 +22,13 @@ classdef PlanarQuadPlant < SecondOrderSystem
       obj = obj.setOutputFrame(obj.getStateFrame);  % allow full-state feedback
     end
     
-    function [xdot, df] = dynamics(obj,t,x,u)
+    function xdot = dynamics(obj,t,x,u)
       % Implement the second-order dynamics
       
-      if (nargout>1)
-        [df]= dynamicsGradients(obj,t,x,u,nargout-1);
-      end
+      % Also add back in [ , df] as second argout
+      %if (nargout>1)
+      %  [df]= dynamicsGradients(obj,t,x,u,nargout-1);
+      %end
       
       phi = x(3);
       
@@ -36,29 +37,29 @@ classdef PlanarQuadPlant < SecondOrderSystem
       w1 = u(1);
       w2 = u(2);
       
-      qdd = [ -sin(q(3))/obj.m*(u(1)+u(2));
-        -obj.g + cos(q(3))/obj.m*(u(1)+u(2));
+      qdd = [ -sin(x(3))/obj.m*(u(1)+u(2));
+        -obj.g + cos(x(3))/obj.m*(u(1)+u(2));
         obj.L/obj.I*(-u(1)+u(2))];
-    
-       
-    
-    
+      
+      qd = x(4:6);
+      xdot = [qd;qdd;1];
+      
     end
     
     function x = getInitialState(obj)
-      x = randn(6,1);
+      x = randn(7,1);
     end
     
     function [c,V] = hoverLQR(obj)
-      x0 = Point(obj.getStateFrame,zeros(6,1));
+      x0 = Point(obj.getStateFrame,zeros(7,1));
       u0 = Point(obj.getInputFrame,obj.m*obj.g/2 * [1;1]);
-      Q = diag([10 10 10 1 1 (obj.L/2/pi)]);  %Q = diag([10*ones(1,3) ones(1,3)]);
+      Q = diag([10 10 10 1 1 (obj.L/2/pi) 1]);  %Q = diag([10*ones(1,3) ones(1,3)]); % Note the 7 states
       R = [0.1 0.05; 0.05 0.1];  %R = diag([0.1 0.1]);
-
+      
       if (nargout>1)
         [c,V0] = tilqr(obj,x0,u0,Q,R);
         sys = feedback(obj,c);
-
+        
         pp = sys.taylorApprox(0,x0,[],3);  % make polynomial approximation
         options=struct();
         options.degL1=2;
