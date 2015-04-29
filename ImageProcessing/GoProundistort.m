162751bce6efaa9279bc9ae4fe05b278b011f453
peteflorence <pete.florence@gmail.com>
2015-04-29 17:16:32 -0400

    undistort directory

--- ImageProcessing/GoProundistort.m
+++ ImageProcessing/GoProundistort.m
@@ -1,14 +1,29 @@
+start = 200;
+last = 450;
+
 load('cameraParams_1080_120_W.mat');
 
-%vid = VideoReader('name_of_video');
-%im = read(vid,ind_frame);
+for i = start:last
+  
+  
+  %vid = VideoReader('name_of_video');
+  %im = read(vid,ind_frame);
+  
+  inpath = '/Users/pflomacpro/ProjectWind/ImageProcessing/in_imagefolder/';
+  imnamein = strcat(inpath,'videoframe',num2str(i),'.bmp');
+  im = imread(imnamein);
+  
+  im_undistorted = undistortImage(im, cameraParameters);
+  
+  outpath = '/Users/pflomacpro/ProjectWind/ImageProcessing/out_imagefolder/';
+  imnameout = strcat(outpath,'UDvideoframe',num2str(i),'.bmp');
+  imwrite(im_undistorted,imnameout,'BMP');
+  
+  
+  %imshow(im)
+  %figure
+  %imshow(im_undistorted)
+  
+end
 
-im = imread('videoframe1.bmp');
-im_undistorted = undistortImage(im, cameraParameters);
-imwrite(im_undistorted,'videoframe1-UD','BMP');
-
-
-%imshow(im)
-%figure
-%imshow(im_undistorted)
 
