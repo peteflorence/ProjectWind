a3bfa74b1f75efd859d49f57b3eb0b2be882e566
peteflorence <pete.florence@gmail.com>
2015-05-12 14:24:34 -0400

    increased automation for image pipeline

--- ImageProcessing/GoProundistort.m
+++ ImageProcessing/GoProundistort.m
@@ -1,29 +1,44 @@
-start = 1;
-last = 1317;
+%start = 1;
+%last = 1317;
 
 load('cameraParams_1080_120_W.mat');
 
-for i = start:last
+%for i = start:last
+i = 0;
+nextframeexist = 2;
+
+for folder = 4:26
   
+  nextframeexist = 2;
   
-  %vid = VideoReader('name_of_video');
-  %im = read(vid,ind_frame);
-  
-  inpath = '/Users/pflomacpro/ProjectWind/ImageProcessing/0430vortex/in_imagefolder/';
-  imnamein = strcat(inpath,'videoframe',num2str(i),'.jpg');
-  im = imread(imnamein);
-  
-  im_undistorted = undistortImage(im, cameraParameters);
-  
-  outpath = '/Users/pflomacpro/ProjectWind/ImageProcessing/0430vortex/out_imagefolder/';
-  imnameout = strcat(outpath,'UDvideoframe',num2str(i),'.bmp');
-  imwrite(im_undistorted,imnameout,'BMP');
-  
-  
-  %imshow(im)
-  %figure
-  %imshow(im_undistorted)
+  while nextframeexist ~= 0
+    
+    i = i+1;
+    %vid = VideoReader('name_of_video');
+    %im = read(vid,ind_frame);
+    
+    inpath = strcat('/Users/pflomacpro/ProjectWind/ImageProcessing/0423vortex/',num2str(folder),'/in_imagefolder/');
+    imnamein = strcat(inpath,'videoframe',num2str(i),'.bmp');
+    im = imread(imnamein);
+    
+    im_undistorted = undistortImage(im, cameraParameters);
+    
+    outpath = strcat('/Users/pflomacpro/ProjectWind/ImageProcessing/0423vortex/',num2str(folder),'/out_imagefolder/');
+    imnameout = strcat(outpath,'UDvideoframe',num2str(i),'.bmp');
+    imwrite(im_undistorted,imnameout,'BMP');
+    
+    
+    %imshow(im)
+    %figure
+    %imshow(im_undistorted)
+    
+    
+    nextframe = strcat(inpath,'videoframe',num2str(i+1),'.bmp');
+    nextframeexist = exist(nextframe, 'file');
+    
+  end
   
 end
+
 
 
