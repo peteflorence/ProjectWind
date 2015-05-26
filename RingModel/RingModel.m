93929e55f94d3d28c384496be15d45fd3c51bad6
peteflorence <pete.florence@gmail.com>
2015-05-26 11:38:58 -0400

    fitted velocity

--- RingModel/RingModel.m
+++ RingModel/RingModel.m
@@ -17,3 +17,31 @@ R = (3 * (R_0)^2 * L / (4 * gamma) ) ^ (1/3)
 % Calculate semi-minor axis
 
 Rgamma = R * gamma
+
+
+
+% Fit velocity
+
+load /Users/pflomacpro/ProjectWind/ImageProcessing/0423vortex/1/data.mat
+
+figure
+plot(data(:,10),data(:,11),'-ko','MarkerSize', 12);
+title('Velocity estimate (finite difference)')
+xlabel('time (seconds)') % x-axis label
+ylabel('velocity (meters / second)') % y-axis label
+
+
+% Dumb guesses
+V_0 = 3.5 % m/s guess
+c = 0.1 % guess
+
+% Initialize column for estimated velocity
+data(:,11) = zeros(length(data(:,8)),1);
+for i = 2:length(data(:,8))
+  t = data(i,10);
+  V = V_0 / (1 + V_0 * c * t);
+  data(i,11) = V;
+end
+hold on
+plot(data(:,10),data(:,11),'-r','MarkerSize', 5);
+
