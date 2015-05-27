7cb12e12269d1cec52bb69b0428350f51710d999
peteflorence <pete.florence@gmail.com>
2015-05-27 09:36:36 -0400

    test 2

--- Drake/runPennDircolWObs.m
+++ Drake/runPennDircolWObs.m
@@ -1,9 +1,5 @@
 function [utraj,xtraj,prog,r] = runPennDircolWObs
 
-% simple planning demo which takes the quadrotor from hover at x=0m to a new hover at
-% x=2m with minimal thrust.
-
-
 %plant = 'russ';
 plant = 'penn';
 
