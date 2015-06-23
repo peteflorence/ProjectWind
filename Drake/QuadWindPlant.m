cb5fbe7b9abed056b7e023572a5f96442f8bddae
peteflorence <pete.florence@gmail.com>
2015-06-23 17:16:46 -0400

    tvWindDraw from ellipsical hypertan, wind dynamics in QuadWindPlant from ellpsical hypertan

--- Drake/QuadWindPlant.m
+++ Drake/QuadWindPlant.m
@@ -245,285 +245,246 @@ classdef QuadWindPlant < DrakeSystem
         
       end
       
+      
+      if strcmp(windfield, 'flyingsphere')
+        V_0 = 3.5; % m/s guess
+        c = 0.1; % guess
+        V = V_0 / (1 + V_0 * c * mytime);
         
-        if strcmp(windfield, 'flyingsphere')
-          V_0 = 3.5; % m/s guess
-          c = 0.1; % guess
-          V = V_0 / (1 + V_0 * c * mytime);
-          
-          obj.ellipsoidcenter = [3 0 1];
-          obj.ellipsoidcenter = obj.ellipsoidcenter - [V*mytime 0 0];
-          xcenter = obj.ellipsoidcenter(1);
-          ycenter = obj.ellipsoidcenter(2);
-          zcenter = obj.ellipsoidcenter(3);
-          
-          sphereradius = 0.30;
-          
-          nomwind = -1.0;
-          
-          xwind = 0;
-          ywind = 0;
-          zwind = 0;
-          
-          
-          
-          
-          reversed = -1;
-          shift1 = -radius;
-          y1 = scale * (tanh(reversed * (x-shift1) * 10 ) +1) / 2;
-          
-          shift2 = 1;
-          y2 = - scale * (tanh(reversed * (x-shift2) * 10 ) +1) / 2;
-          
-          y = y1 + y2;
-          
-          
-          
-          
-          if (xquad - xcenter)^2/sphereradius^2 + (yquad-ycenter)^2/sphereradius^2 + (zquad - zcenter)^2/sphereradius^2 < 1
-            xwind = nomwind;
-          elseif (xquad - xcenter)^2/extsphere^2 + (yquad-ycenter)^2/extsphere^2 + (zquad - zcenter)^2/extsphere^2 < 1
-            display('found a boundary point')
-            
-            % hypertan wants to go from -1 to 1
-            % this f(x) goes from 0 to 3, as x goes from -1 to 1
-            
-            % find distance to edge of sphere
-            fromcenter = sqrt( (xquad - xcenter)^2 + (yquad-ycenter)^2 + (zquad - zcenter)^2 );
-            distance = fromcenter - sphereradius;
-            
-            
-            % normalize that distance
-            normdistance = distance / boundary;
-            % make it go from -1 to 1
-            hypertandistance = normdistance * 2 - 1;
-            
-            % in this setup, -1 is right next to the sphere, and 1 is out in
-            % nowhere
-            
-            reversed = -1;
-            xwind = nomwind * (tanh(reversed * hypertandistance * 10)+1)/2;
-            
-          else
-            xwind = 0;
-          end
-          
-        end
+        obj.ellipsoidcenter = [3 0 1];
+        obj.ellipsoidcenter = obj.ellipsoidcenter - [V*mytime 0 0];
+        xcenter = obj.ellipsoidcenter(1);
+        ycenter = obj.ellipsoidcenter(2);
+        zcenter = obj.ellipsoidcenter(3);
         
-          
-          
-          
-          
-          
-
-          
-          
-          
-          
-          
+        sphereRadius = 0.30;
         
-        if strcmp(windfield, 'zero')
-          ywind = 0;
-        elseif strcmp(windfield, 'constant')
-          ywind = 2;
-        elseif strcmp(windfield, 'linear')
-          ywind = zquad;
-        elseif strcmp(windfield, 'quadratic')
-          ywind = zquad^2;
-        elseif strcmp(windfield, 'sqrt')
-          ywind = (abs(zquad))^(1/2);
-        elseif strcmp(windfield, 'exp')
-          a = 1;
-          C=20/exp(6.5);
-          b=-1;
-          d = 0;
-          %z  = b + C*exp(a*ydotdot);
-          ywind = 1/a*log((zquad-b)/C) - d;
-        elseif strcmp(windfield, 'difftailhead')
-          ywind = 0;
-        elseif strcmp(windfield, 'tvsin')
-          ywind = -10*sin(10*mytime);
-        elseif strcmp(windfield, 'tlinear')
-          ywind = -5 - mytime;
-        else
-          %disp('Please specify which kind of wind field!')
-        end
+        nomwind = -1.0;
         
+        xwind = 0;
+        ywind = 0;
+        zwind = 0;
         
+        xidif = xquad - xcenter;
+        yidif = yquad - ycenter;
+        zidif = zquad - zcenter; 
         
-        wind = [xwind;ywind;zwind];
+        scale = nomwind;
         
+        reversed = -1;
         
-        dquadinwind = sparse(13,18);
+        a = sqrt(xidif^2 + yidif^2 + zidif^2);
         
-        %       if strcmp(windfield, 'zero')
-        %         ;
-        %       elseif strcmp(windfield, 'flyingellipsoid')
-        %         ;
-        %       elseif strcmp(windfield, 'constant')
-        %         ;
-        %       elseif strcmp(windfield, 'thermals')
-        %         ;
-        %       elseif strcmp(windfield, 'linear')
-        %         dquadinwind(8,4) = 1/obj.m;
-        %       elseif strcmp(windfield, 'quadratic')
-        %         dquadinwind(8,4) = 2*zquad/obj.m;
-        %       elseif strcmp(windfield, 'sqrt')
-        %         ywind = 1/2*(abs(zquad))^(-1/2);
-        %       elseif strcmp(windfield, 'exp')
-        %         dquadinwind(8,4) = 1/a*1/((zquad-b)/C)/obj.m;
-        %       elseif strcmp(windfield, 'difftailhead')
-        %         dquadinwind(7,3) = 10*cos(yquad)/obj.m;
-        %       elseif strcmp(windfield, 'tvsin')
-        %         dquadinwind(8,1) = -10*cos(10*mytime)/obj.m;
-        %       elseif strcmp(windfield, 'tlinear')
-        %         dquadinwind(8,1) = -1/obj.m;
-        %         %else
-        %         %  disp('Please specify which kind of wind field!')
-        %       end
+        slope = 10;
         
-        
-        % I think all this code can go... I've figured out drawing elsewhere
-        
-        %       lcmgl = drake.util.BotLCMGLClient(lcm.lcm.LCM.getSingleton(), 'Windy');
-        %       lcmgl.glColor3f(0,1,0);
-        %
-        %       if plotme == 1 && strcmp(windfield, 'flyingellipsoid')
-        %         xcenter = obj.ellipsoidcenter(1);
-        %         ycenter = obj.ellipsoidcenter(2);
-        %         zcenter = obj.ellipsoidcenter(3);
-        %         for yi = (ycenter-ellipsoidmajor):0.05:(ycenter+ellipsoidmajor)
-        %           for xi = (xcenter-ellipsoidminor):0.05:(xcenter+ellipsoidminor)
-        %             for zi = (zcenter-ellipsoidmajor):0.05:(zcenter+ellipsoidmajor)
-        %               ywind = 0;
-        %               zwind = 0;
-        %               if (xi - xcenter)^2/ellipsoidminor^2 + (yi-ycenter)^2/ellipsoidmajor^2 + (zi - zcenter)^2/ellipsoidmajor^2 < 1
-        %                 xwind = -0.6;
-        %               else
-        %                 xwind = 0;
-        %               end
-        %               pos = [xi, yi, zi];
-        %               force = [xwind, ywind, zwind];
-        %               lcmgl.drawVector3d(pos,force/20);
-        %             end
-        %
-        %           end
-        %         end
-        %       elseif plotme == 1
-        %         for xi = 1:10
-        %           %for yi = 1:10
-        %           for zi = 1:10
-        %             pos = [xi, 0, zi];
-        %             force = [xwind, ywind, zwind];
-        %             %lcmgl.drawVector3d([0,0,0],[1,1,1]);
-        %             lcmgl.drawVector3d(pos,force);
-        %           end
-        %
-        %         end
-        %
-        %
-        %
-        %       end
-        %       lcmgl.glEnd();
-        %       lcmgl.switchBuffers;
-        
+        xwind = scale * (tanh(reversed * ( a - sphereRadius) * slope ) +1) / 2;
         
       end
       
       
-      function y = output(obj,t,x,u)
-        y = x;
+      
+      if strcmp(windfield, 'zero')
+        ywind = 0;
+      elseif strcmp(windfield, 'constant')
+        ywind = 2;
+      elseif strcmp(windfield, 'linear')
+        ywind = zquad;
+      elseif strcmp(windfield, 'quadratic')
+        ywind = zquad^2;
+      elseif strcmp(windfield, 'sqrt')
+        ywind = (abs(zquad))^(1/2);
+      elseif strcmp(windfield, 'exp')
+        a = 1;
+        C=20/exp(6.5);
+        b=-1;
+        d = 0;
+        %z  = b + C*exp(a*ydotdot);
+        ywind = 1/a*log((zquad-b)/C) - d;
+      elseif strcmp(windfield, 'difftailhead')
+        ywind = 0;
+      elseif strcmp(windfield, 'tvsin')
+        ywind = -10*sin(10*mytime);
+      elseif strcmp(windfield, 'tlinear')
+        ywind = -5 - mytime;
+      else
+        %disp('Please specify which kind of wind field!')
       end
       
-      function x = getInitialState(obj)
-        x = zeros(13,1);
-      end
       
-      function obj = addTrees(obj,number_of_obstacles)
-        % Adds a random forest of trees
-        if nargin<2, number_of_obstacles = 5*(randi(5)+2); end
-        for i=1:number_of_obstacles
-          % Populates an area of the forest
-          xy = [20,0;0,12]*(rand(2,1) - [0.5;0]);
-          % Creates a clear path through the middle of the forest
-          while(norm(xy)<1 || (xy(1,1)<=1.5 && xy(1,1)>=-1.5)), xy = randn(2,1); end
-          height = 1+rand;
-          width_param = rand(1,2);
-          yaw = randn;
-          obj = obj.addTree([width_param height],xy,yaw);
-        end
-        obj = compile(obj);
-      end
       
-      function obj = addTree(obj, lwh, xy, yaw)
-        % Adds a single tree with specified length width height, xy
-        % location, and yaw orientation.
-        height = lwh(1,3);
-        width_param = lwh(1,1:2);
-        treeTrunk = RigidBodyBox([.2+.8*width_param height],...
-          [xy;height/2],[0;0;yaw]);
-        treeTrunk.c = [83,53,10]/255;  % brown
-        obj = addGeometryToBody(obj,'world',treeTrunk);
-        treeLeaves = RigidBodyBox(1.5*[.2+.8*width_param height/4],...
-          [xy;height + height/8],[0;0;yaw]);
-        treeLeaves.c = [0,0.7,0];  % green
-        obj = addGeometryToBody(obj,'world',treeLeaves);
-        obj = compile(obj);
-      end
+      wind = [xwind;ywind;zwind];
       
-      function traj_opt = addPlanVisualizer(obj,traj_opt)
-        % spew out an lcmgl visualization of the trajectory.  intended to be
-        % used as a callback (fake objective) in the direct trajectory
-        % optimization classes
-        
-        if ~checkDependency('lcmgl')
-          warning('lcmgl dependency is missing.  skipping visualization');
-          return;
-        end
-        lcmgl = drake.util.BotLCMGLClient(lcm.lcm.LCM.getSingleton(), 'QuadrotorPlan');
-        
-        typecheck(traj_opt,'DirectTrajectoryOptimization');
-        
-        traj_opt = traj_opt.addDisplayFunction(@(x)visualizePlan(x,lcmgl),traj_opt.x_inds(1:3,:));
-        
-        function visualizePlan(x,lcmgl)
-          lcmgl.glColor3f(1, 0, 0);
-          lcmgl.glPointSize(3);
-          lcmgl.points(x(1,:),x(2,:),x(3,:));
-          lcmgl.glColor3f(1, .5, 0);
-          lcmgl.plot3(x(1,:),x(2,:),x(3,:));
-          lcmgl.switchBuffers;
-          
-          
-          
-          
-          
-          
-        end
-        
-        
-        
-        
-        
-      end
+      
+      dquadinwind = sparse(13,18);
+      
+      %       if strcmp(windfield, 'zero')
+      %         ;
+      %       elseif strcmp(windfield, 'flyingellipsoid')
+      %         ;
+      %       elseif strcmp(windfield, 'constant')
+      %         ;
+      %       elseif strcmp(windfield, 'thermals')
+      %         ;
+      %       elseif strcmp(windfield, 'linear')
+      %         dquadinwind(8,4) = 1/obj.m;
+      %       elseif strcmp(windfield, 'quadratic')
+      %         dquadinwind(8,4) = 2*zquad/obj.m;
+      %       elseif strcmp(windfield, 'sqrt')
+      %         ywind = 1/2*(abs(zquad))^(-1/2);
+      %       elseif strcmp(windfield, 'exp')
+      %         dquadinwind(8,4) = 1/a*1/((zquad-b)/C)/obj.m;
+      %       elseif strcmp(windfield, 'difftailhead')
+      %         dquadinwind(7,3) = 10*cos(yquad)/obj.m;
+      %       elseif strcmp(windfield, 'tvsin')
+      %         dquadinwind(8,1) = -10*cos(10*mytime)/obj.m;
+      %       elseif strcmp(windfield, 'tlinear')
+      %         dquadinwind(8,1) = -1/obj.m;
+      %         %else
+      %         %  disp('Please specify which kind of wind field!')
+      %       end
+      
+      
+      % I think all this code can go... I've figured out drawing elsewhere
+      
+      %       lcmgl = drake.util.BotLCMGLClient(lcm.lcm.LCM.getSingleton(), 'Windy');
+      %       lcmgl.glColor3f(0,1,0);
+      %
+      %       if plotme == 1 && strcmp(windfield, 'flyingellipsoid')
+      %         xcenter = obj.ellipsoidcenter(1);
+      %         ycenter = obj.ellipsoidcenter(2);
+      %         zcenter = obj.ellipsoidcenter(3);
+      %         for yi = (ycenter-ellipsoidmajor):0.05:(ycenter+ellipsoidmajor)
+      %           for xi = (xcenter-ellipsoidminor):0.05:(xcenter+ellipsoidminor)
+      %             for zi = (zcenter-ellipsoidmajor):0.05:(zcenter+ellipsoidmajor)
+      %               ywind = 0;
+      %               zwind = 0;
+      %               if (xi - xcenter)^2/ellipsoidminor^2 + (yi-ycenter)^2/ellipsoidmajor^2 + (zi - zcenter)^2/ellipsoidmajor^2 < 1
+      %                 xwind = -0.6;
+      %               else
+      %                 xwind = 0;
+      %               end
+      %               pos = [xi, yi, zi];
+      %               force = [xwind, ywind, zwind];
+      %               lcmgl.drawVector3d(pos,force/20);
+      %             end
+      %
+      %           end
+      %         end
+      %       elseif plotme == 1
+      %         for xi = 1:10
+      %           %for yi = 1:10
+      %           for zi = 1:10
+      %             pos = [xi, 0, zi];
+      %             force = [xwind, ywind, zwind];
+      %             %lcmgl.drawVector3d([0,0,0],[1,1,1]);
+      %             lcmgl.drawVector3d(pos,force);
+      %           end
+      %
+      %         end
+      %
+      %
+      %
+      %       end
+      %       lcmgl.glEnd();
+      %       lcmgl.switchBuffers;
+      
       
     end
     
-    properties
-      m = .5;
-      I = diag([0.0023,0.0023,0.004]);
+    
+    function y = output(obj,t,x,u)
+      y = x;
+    end
+    
+    function x = getInitialState(obj)
+      x = zeros(13,1);
+    end
+    
+    function obj = addTrees(obj,number_of_obstacles)
+      % Adds a random forest of trees
+      if nargin<2, number_of_obstacles = 5*(randi(5)+2); end
+      for i=1:number_of_obstacles
+        % Populates an area of the forest
+        xy = [20,0;0,12]*(rand(2,1) - [0.5;0]);
+        % Creates a clear path through the middle of the forest
+        while(norm(xy)<1 || (xy(1,1)<=1.5 && xy(1,1)>=-1.5)), xy = randn(2,1); end
+        height = 1+rand;
+        width_param = rand(1,2);
+        yaw = randn;
+        obj = obj.addTree([width_param height],xy,yaw);
+      end
+      obj = compile(obj);
+    end
+    
+    function obj = addTree(obj, lwh, xy, yaw)
+      % Adds a single tree with specified length width height, xy
+      % location, and yaw orientation.
+      height = lwh(1,3);
+      width_param = lwh(1,1:2);
+      treeTrunk = RigidBodyBox([.2+.8*width_param height],...
+        [xy;height/2],[0;0;yaw]);
+      treeTrunk.c = [83,53,10]/255;  % brown
+      obj = addGeometryToBody(obj,'world',treeTrunk);
+      treeLeaves = RigidBodyBox(1.5*[.2+.8*width_param height/4],...
+        [xy;height + height/8],[0;0;yaw]);
+      treeLeaves.c = [0,0.7,0];  % green
+      obj = addGeometryToBody(obj,'world',treeLeaves);
+      obj = compile(obj);
+    end
+    
+    function traj_opt = addPlanVisualizer(obj,traj_opt)
+      % spew out an lcmgl visualization of the trajectory.  intended to be
+      % used as a callback (fake objective) in the direct trajectory
+      % optimization classes
       
-      % defining gravity here (too simple?)
-      gravity = [0; 0; -9.8100]
+      if ~checkDependency('lcmgl')
+        warning('lcmgl dependency is missing.  skipping visualization');
+        return;
+      end
+      lcmgl = drake.util.BotLCMGLClient(lcm.lcm.LCM.getSingleton(), 'QuadrotorPlan');
+      
+      typecheck(traj_opt,'DirectTrajectoryOptimization');
+      
+      traj_opt = traj_opt.addDisplayFunction(@(x)visualizePlan(x,lcmgl),traj_opt.x_inds(1:3,:));
+      
+      function visualizePlan(x,lcmgl)
+        lcmgl.glColor3f(1, 0, 0);
+        lcmgl.glPointSize(3);
+        lcmgl.points(x(1,:),x(2,:),x(3,:));
+        lcmgl.glColor3f(1, .5, 0);
+        lcmgl.plot3(x(1,:),x(2,:),x(3,:));
+        lcmgl.switchBuffers;
+        
+        
+        
+        
+        
+        
+      end
       
       
-      windfield = 'zero'; % zero windfield by default
       
-      ellipsoidcenter = [0 0 0];
-      tstep = 0.01;
+      
+      
     end
     
   end
   
+  properties
+    m = .5;
+    I = diag([0.0023,0.0023,0.004]);
+    
+    % defining gravity here (too simple?)
+    gravity = [0; 0; -9.8100]
+    
+    
+    windfield = 'zero'; % zero windfield by default
+    
+    ellipsoidcenter = [0 0 0];
+    tstep = 0.01;
+  end
   
-  
+end
+
+
+
