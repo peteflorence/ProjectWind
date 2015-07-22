a7d768525901ff60ed1bc9ec70502c0b82e9cbb3
peteflorence <pete.florence@gmail.com>
2015-07-22 16:46:13 -0400

    runPennDircolWind_numerical now checking lcmgl dependency correctly

--- Drake/WindVisualizer.m
+++ Drake/WindVisualizer.m
@@ -2,13 +2,14 @@ classdef WindVisualizer < BotVisualizer
 % Implements the draw function for the Acrobot 
 
   methods
-    function obj = WindVisualizer(plant)
+    function obj = WindVisualizer(plant, lcmgl)
       % Construct visualizer
       %   AcrobotVisualizer(AcrobotPlant) will take the necessary
       %   parameters from the plant class
       
       %typecheck(plant,'AcrobotPlant');
       obj = obj@BotVisualizer(plant);
+      obj.lcmgl = lcmgl;
       %obj.l1 = plant.l1;
       %obj.l2 = plant.l2;
     end
@@ -16,14 +17,15 @@ classdef WindVisualizer < BotVisualizer
     function draw(obj,t,x)
       
       draw@BotVisualizer(obj,t,x)
-      
-      
-      tvWindDraw_FlyingSphere_noLogic(t);
+      if ~isempty(obj.lcmgl)
+        tvWindDraw_FlyingSphere_noLogic(t, obj.lcmgl);
+      end
       
     end
   end
 
   properties
+    lcmgl;
 
   end
   
