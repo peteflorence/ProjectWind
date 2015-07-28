1861a8ff43c863157b1c26f6c29692e22828dbc1
peteflorence <pete.florence@gmail.com>
2015-07-28 12:43:44 -0400

    Cpp and M versions

--- Drake/@QuadWindPlant_numericalCpp/QuadWindPlant_numerical.m
+++ Drake/@QuadWindPlant_numericalCpp/QuadWindPlant_numerical.m
@@ -50,135 +50,135 @@ classdef QuadWindPlant_numerical < DrakeSystem
     
     
     
-    function xdot = dynamics_no_grad(obj,t,x,u)
-      % States
-      % x
-      % y
-      % z
-      % phi (roll)
-      % theta (pitch)
-      % psi (yaw)
-      % xdot
-      % ydot
-      % zdot
-      % phidot
-      % thetadot
-      % psidot
-      % time
-      
-      
-      % Parameters
-      m = obj.m;
-      I = obj.I;
-      invI = obj.invI;
-      g = 9.81;
-      L = 0.1750;
-      
-      % states
-      phi = x(4);
-      theta = x(5);
-      psi = x(6);
-      
-      phidot = x(10);
-      thetadot = x(11);
-      psidot = x(12);
-      
-      w1 = u(1);
-      w2 = u(2);
-      w3 = u(3);
-      w4 = u(4);
-      
-      % Rotation matrix from body to world frames
-      R = rpy2rotmat([phi;theta;psi]);
-      
-      kf = 1; % 6.11*10^-8;
-      
-      F1 = kf*w1;
-      F2 = kf*w2;
-      F3 = kf*w3;
-      F4 = kf*w4;
-      
-      km = 0.0245;
-      
-      M1 = km*w1;
-      M2 = km*w2;
-      M3 = km*w3;
-      M4 = km*w4;
-      
-      xquad = x(1);
-      yquad = x(2);
-      zquad = x(3);
-      quadpos = [xquad;yquad;zquad];
-      
-      
-      windout = obj.quadwind(quadpos,x(13),1); % pass mytime to quadwind. % last arument is plot option
-      
-      xyz_ddot = (1/m)*([0;0;-m*g] + R*[0;0;F1+F2+F3+F4] + windout); % call to wind field in dynamics
-      
-      pqr = rpydot2angularvel([phi;theta;psi],[phidot;thetadot;psidot]);
-      pqr = R'*pqr;
-      
-      pqr_dot = invI*([L*(F2-F4);L*(F3-F1);(M1-M2+M3-M4)] - cross(pqr,I*pqr));
-      
-      % Now, convert pqr_dot to rpy_ddot
-      [Phi, dPhi] = angularvel2rpydotMatrix([phi;theta;psi]);
-      
-      Rdot =  [ 0, sin(phi)*sin(psi) + cos(phi)*cos(psi)*sin(theta),   cos(phi)*sin(psi) - cos(psi)*sin(phi)*sin(theta); ...
-        0, cos(phi)*sin(psi)*sin(theta) - cos(psi)*sin(phi), - cos(phi)*cos(psi) - sin(phi)*sin(psi)*sin(theta); ...
-        0,                              cos(phi)*cos(theta),                               -cos(theta)*sin(phi)]*phidot + ...
-        [ -cos(psi)*sin(theta), cos(psi)*cos(theta)*sin(phi), cos(phi)*cos(psi)*cos(theta); ...
-        -sin(psi)*sin(theta), cos(theta)*sin(phi)*sin(psi), cos(phi)*cos(theta)*sin(psi); ...
-        -cos(theta),         -sin(phi)*sin(theta),         -cos(phi)*sin(theta)]*thetadot + ...
-        [ -cos(theta)*sin(psi), - cos(phi)*cos(psi) - sin(phi)*sin(psi)*sin(theta), cos(psi)*sin(phi) - cos(phi)*sin(psi)*sin(theta); ...
-        cos(psi)*cos(theta),   cos(psi)*sin(phi)*sin(theta) - cos(phi)*sin(psi), sin(phi)*sin(psi) + cos(phi)*cos(psi)*sin(theta); ...
-        0,                                                  0,                                                0]*psidot;
-      
-      rpy_ddot = Phi*R*pqr_dot + reshape((dPhi*[phidot;thetadot;psidot]),3,3)*R*pqr + ...
-        Phi*Rdot*pqr;
-      
-      qdd = [xyz_ddot;rpy_ddot];
-      qd = x(7:12);
-      xdot = [qd;qdd;1]; % the 1 at the end is for mytime
-      
-    end
-    
-    function wind = quadwind(obj,quadpos,mytime,plotme)
-      xquad = quadpos(1);
-      yquad = quadpos(2);
-      zquad = quadpos(3);
-      windfield = 'flyingsphere';
-      
-      if strcmp(windfield, 'flyingsphere')
-        V_0 = 3.5; % m/s guess
-        c = 0.1; % guess
-        V = V_0 / (1 + V_0 * c * mytime);
-        
-        obj.ellipsoidcenter = [2 0 1];
-        obj.ellipsoidcenter = obj.ellipsoidcenter - [V*mytime 0 0];
-        xcenter = obj.ellipsoidcenter(1);
-        ycenter = obj.ellipsoidcenter(2);
-        zcenter = obj.ellipsoidcenter(3);
-        
-        sphereRadius = 0.30;
-        nomwind = -5.0;
-        
-        xwind = 0;
-        ywind = 0;
-        zwind = 0;
-        
-        xidif = xquad - xcenter;
-        yidif = yquad - ycenter;
-        zidif = zquad - zcenter;
-        
-        scale = nomwind;
-        reversed = -1;
-        a = sqrt(xidif^2 + yidif^2 + zidif^2);
-        slope = 10;
-        xwind = scale * (tanh(reversed * ( a - sphereRadius) * slope ) +1) / 2;
-      end
-      wind = [xwind;ywind;zwind];
-    end
-    
+%     function xdot = dynamics_no_grad(obj,t,x,u)
+%       % States
+%       % x
+%       % y
+%       % z
+%       % phi (roll)
+%       % theta (pitch)
+%       % psi (yaw)
+%       % xdot
+%       % ydot
+%       % zdot
+%       % phidot
+%       % thetadot
+%       % psidot
+%       % time
+%       
+%       
+%       % Parameters
+%       m = obj.m;
+%       I = obj.I;
+%       invI = obj.invI;
+%       g = 9.81;
+%       L = 0.1750;
+%       
+%       % states
+%       phi = x(4);
+%       theta = x(5);
+%       psi = x(6);
+%       
+%       phidot = x(10);
+%       thetadot = x(11);
+%       psidot = x(12);
+%       
+%       w1 = u(1);
+%       w2 = u(2);
+%       w3 = u(3);
+%       w4 = u(4);
+%       
+%       % Rotation matrix from body to world frames
+%       R = rpy2rotmat([phi;theta;psi]);
+%       
+%       kf = 1; % 6.11*10^-8;
+%       
+%       F1 = kf*w1;
+%       F2 = kf*w2;
+%       F3 = kf*w3;
+%       F4 = kf*w4;
+%       
+%       km = 0.0245;
+%       
+%       M1 = km*w1;
+%       M2 = km*w2;
+%       M3 = km*w3;
+%       M4 = km*w4;
+%       
+%       xquad = x(1);
+%       yquad = x(2);
+%       zquad = x(3);
+%       quadpos = [xquad;yquad;zquad];
+%       
+%       
+%       windout = obj.quadwind(quadpos,x(13),1); % pass mytime to quadwind. % last arument is plot option
+%       
+%       xyz_ddot = (1/m)*([0;0;-m*g] + R*[0;0;F1+F2+F3+F4] + windout); % call to wind field in dynamics
+%       
+%       pqr = rpydot2angularvel([phi;theta;psi],[phidot;thetadot;psidot]);
+%       pqr = R'*pqr;
+%       
+%       pqr_dot = invI*([L*(F2-F4);L*(F3-F1);(M1-M2+M3-M4)] - cross(pqr,I*pqr));
+%       
+%       % Now, convert pqr_dot to rpy_ddot
+%       [Phi, dPhi] = angularvel2rpydotMatrix([phi;theta;psi]);
+%       
+%       Rdot =  [ 0, sin(phi)*sin(psi) + cos(phi)*cos(psi)*sin(theta),   cos(phi)*sin(psi) - cos(psi)*sin(phi)*sin(theta); ...
+%         0, cos(phi)*sin(psi)*sin(theta) - cos(psi)*sin(phi), - cos(phi)*cos(psi) - sin(phi)*sin(psi)*sin(theta); ...
+%         0,                              cos(phi)*cos(theta),                               -cos(theta)*sin(phi)]*phidot + ...
+%         [ -cos(psi)*sin(theta), cos(psi)*cos(theta)*sin(phi), cos(phi)*cos(psi)*cos(theta); ...
+%         -sin(psi)*sin(theta), cos(theta)*sin(phi)*sin(psi), cos(phi)*cos(theta)*sin(psi); ...
+%         -cos(theta),         -sin(phi)*sin(theta),         -cos(phi)*sin(theta)]*thetadot + ...
+%         [ -cos(theta)*sin(psi), - cos(phi)*cos(psi) - sin(phi)*sin(psi)*sin(theta), cos(psi)*sin(phi) - cos(phi)*sin(psi)*sin(theta); ...
+%         cos(psi)*cos(theta),   cos(psi)*sin(phi)*sin(theta) - cos(phi)*sin(psi), sin(phi)*sin(psi) + cos(phi)*cos(psi)*sin(theta); ...
+%         0,                                                  0,                                                0]*psidot;
+%       
+%       rpy_ddot = Phi*R*pqr_dot + reshape((dPhi*[phidot;thetadot;psidot]),3,3)*R*pqr + ...
+%         Phi*Rdot*pqr;
+%       
+%       qdd = [xyz_ddot;rpy_ddot];
+%       qd = x(7:12);
+%       xdot = [qd;qdd;1]; % the 1 at the end is for mytime
+%       
+%     end
+%     
+%     function wind = quadwind(obj,quadpos,mytime,plotme)
+%       xquad = quadpos(1);
+%       yquad = quadpos(2);
+%       zquad = quadpos(3);
+%       windfield = 'flyingsphere';
+%       
+%       if strcmp(windfield, 'flyingsphere')
+%         V_0 = 3.5; % m/s guess
+%         c = 0.1; % guess
+%         V = V_0 / (1 + V_0 * c * mytime);
+%         
+%         obj.ellipsoidcenter = [2 0 1];
+%         obj.ellipsoidcenter = obj.ellipsoidcenter - [V*mytime 0 0];
+%         xcenter = obj.ellipsoidcenter(1);
+%         ycenter = obj.ellipsoidcenter(2);
+%         zcenter = obj.ellipsoidcenter(3);
+%         
+%         sphereRadius = 0.30;
+%         nomwind = -5.0;
+%         
+%         xwind = 0;
+%         ywind = 0;
+%         zwind = 0;
+%         
+%         xidif = xquad - xcenter;
+%         yidif = yquad - ycenter;
+%         zidif = zquad - zcenter;
+%         
+%         scale = nomwind;
+%         reversed = -1;
+%         a = sqrt(xidif^2 + yidif^2 + zidif^2);
+%         slope = 10;
+%         xwind = scale * (tanh(reversed * ( a - sphereRadius) * slope ) +1) / 2;
+%       end
+%       wind = [xwind;ywind;zwind];
+%     end
+%     
     
     function y = output(obj,t,x,u)
       y = x;
