71b8f6110081373582587348931f9a4b69e34e75
peteflorence <pete.florence@gmail.com>
2015-07-28 12:55:56 -0400

    Unit test implemeneted for cpp dynamics

--- Drake/UnitTests_QuadWindPlant_numericalCpp.m
+++ Drake/UnitTests_QuadWindPlant_numericalCpp.m
@@ -1,7 +1,25 @@
-cd /Users/pflomacpro/ProjectWind/Drake; 
-r = QuadWindPlant_numerical; 
-r.dynamics_no_grad(1,ones(13,1),ones(4,1))
+cd /Users/pflomacpro/ProjectWind/Drake;
 
-cd /Users/pflomacpro/ProjectWind/Drake; 
-r = QuadWindPlant_numericalCpp; 
-r.dynamics_no_grad(1,ones(13,1),ones(4,1))
+
+
+for i = 1:10
+  
+  t = rand(1)*10;
+  x = rand(13,1);
+  u = rand(4,1);
+  
+  rM = QuadWindPlant_numerical;
+  MATLABans = rM.dynamics_no_grad(t,x,u);
+  
+  rCpp = QuadWindPlant_numericalCpp;
+  Cppans = rCpp.dynamics_no_grad(t,x,u);
+  
+  diff = norm(MATLABans - Cppans);
+  
+  display(diff);
+  
+  if diff > 1e-3
+    pause
+  end
+ 
+end
