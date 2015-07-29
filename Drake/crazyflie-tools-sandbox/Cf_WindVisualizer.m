13129f328bd8eb5160d018f64b21e7d7f7a0e678
peteflorence <pete.florence@gmail.com>
2015-07-29 16:46:32 -0400

    big smiley face. crazyflie model dodging wind gusts with all dynamics in cpp

---
+++ Drake/crazyflie-tools-sandbox/Cf_WindVisualizer.m
@@ -0,0 +1,32 @@
+classdef Cf_WindVisualizer < BotVisualizer
+% Implements the draw function for the Acrobot 
+
+  methods
+    function obj = Cf_WindVisualizer(plant, lcmgl)
+      % Construct visualizer
+      %   AcrobotVisualizer(AcrobotPlant) will take the necessary
+      %   parameters from the plant class
+      
+      %typecheck(plant,'AcrobotPlant');
+      obj = obj@BotVisualizer(plant);
+      obj.lcmgl = lcmgl;
+      %obj.l1 = plant.l1;
+      %obj.l2 = plant.l2;
+    end
+    
+    function draw(obj,t,x)
+      
+      draw@BotVisualizer(obj,t,x)
+      if ~isempty(obj.lcmgl)
+        Cf_tvWindDraw_FlyingSphere_noLogic(t, obj.lcmgl);
+      end
+      
+    end
+  end
+
+  properties
+    lcmgl;
+
+  end
+  
+end
---
+++ Drake/crazyflie-tools-sandbox/Cf_tvWindDraw_FlyingSphere_noLogic.m
@@ -0,0 +1,76 @@
+function Cf_tvWindDraw_FlyingSphere_noLogic(t, lcmgl)
+
+ellipsoidcenter = [2 0 1];
+
+windfield = 'flyingsphere';
+
+V_0 = 3.5; % m/s guess
+c = 0.1; % guess
+V = V_0 / (1 + V_0 * c * t);
+%V = V_0;
+
+ellipsoidcenter = ellipsoidcenter - [V*t 0 0];
+
+sphereRadius = 0.3;
+boundary = 0.1;
+extSphere = sphereRadius + boundary;
+
+
+
+
+step = 0.10;
+nomwind = -3.0;
+
+if strcmp(windfield, 'flyingsphere')
+  xcenter = ellipsoidcenter(1);
+  ycenter = ellipsoidcenter(2);
+  zcenter = ellipsoidcenter(3);
+  
+  
+  for yi = (ycenter-extSphere):step:(ycenter+extSphere)
+    yidif = yi - ycenter;          % translate
+    for xi = (xcenter-extSphere):step:(xcenter+extSphere)
+      xidif = xi - xcenter;        % translate
+      for zi = (zcenter-extSphere):step:(zcenter+extSphere)
+        zidif = zi - zcenter;      % translate
+        
+        
+        xwind = 0;
+        ywind = 0;
+        zwind = 0;
+        
+
+        % cart2sph
+        %[azimuth,elevation,r] = cart2sph(xidif,yidif,zidif);
+        
+
+        scale = nomwind;
+        
+        reversed = -1;
+        
+        a = sqrt(xidif^2 + yidif^2 + zidif^2);
+        
+        slope = 10;
+        
+        xwind = scale * (tanh(reversed * ( a - sphereRadius) * slope ) +1) / 2;
+        
+        if abs(xwind) > 0.1
+          pos = [xi, yi, zi];
+          force = [xwind, ywind, zwind];
+          lcmgl.glColor3f(0,1,0);
+          lcmgl.drawVector3d(pos,force/20);
+        end
+      end
+      
+    end
+  end
+end
+
+
+lcmgl.glEnd();
+lcmgl.switchBuffers();
+pause(0.01)
+end
+
+
+
