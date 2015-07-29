507ab8736451eea51d241c1b8b696a6275268b8f
peteflorence <pete.florence@gmail.com>
2015-07-29 11:44:55 -0400

    drpy2rotmat matrix

--- Drake/crazyflie-tools-sandbox/UnitTests_CrazyFlieWindDynamics.m
+++ Drake/crazyflie-tools-sandbox/UnitTests_CrazyFlieWindDynamics.m
@@ -3,7 +3,9 @@ function UnitTests_CrazyFlieWindDynamics
 cd /Users/pflomacpro/ProjectWind/Drake/crazyflie-tools-sandbox;
 
 s = rng(123512, 'twister');
-for i = 1:20
+numiter = 5;
+r_ddot = zeros(numiter,1);
+for i = 1:numiter
   
 
   t = rand(1)*1;
@@ -13,10 +15,12 @@ for i = 1:20
   
   rBen = CrazyflieModel;
   Benans = rBen.manip.dynamics(t,x12,u);
+  display('Ben gives'); display(Benans);
   
   rPete = CrazyflieWindModel;
   Peteans = rPete.dynamics_no_grad(t,x13,u);
   Peteans12 = Peteans(1:12);
+  display('Pete gives'); display(Peteans12);
   
   diffvec = Benans - Peteans12;
   diff = norm(diffvec);
@@ -29,16 +33,19 @@ for i = 1:20
     %display('x13: '); display(x13);
     %display('u: '); display(u);
      
-    display('Ben gives'); display(Benans);
-    display('Pete gives'); display(Peteans12);
-    
     xdotlabel = linspace(1,12,12);
     figure;
     plot(xdotlabel, diffvec, 'rX')
   end
   
+  r_ddot(i) = Benans(10)/Peteans(10);
+  
+  
+  
  
 end
 rng(s);
+iterlabel = linspace(1,numiter,numiter);
+plot(iterlabel,r_ddot);
 
 end
