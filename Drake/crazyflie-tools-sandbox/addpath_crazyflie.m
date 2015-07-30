197eded689961972e420a222c2524dbd6a1c12d9
peteflorence <pete.florence@gmail.com>
2015-07-30 16:51:41 -0400

    runtvlqrWind is the script I need tomorrow

--- Drake/crazyflie-tools-sandbox/addpath_crazyflie.m
+++ Drake/crazyflie-tools-sandbox/addpath_crazyflie.m
@@ -1,6 +1,6 @@
 function addpath_crazyflie()
-    javaaddpath('../vicon/src/vicon_t.jar');
-    javaaddpath('../lcm/crazyflie_t.jar');
+    javaaddpath('/Users/pflomacpro/crazyflie-tools/vicon/src/vicon_t.jar');
+    javaaddpath('/Users/pflomacpro/crazyflie-tools/lcm/crazyflie_t.jar');
     addpath('coders');
     %addpath_mosek
     %addpath_iris
